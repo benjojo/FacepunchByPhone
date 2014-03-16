666b55de20f88c36235b10fadc6e74944ccc7a19
Ben Cox <ben@benjojo.co.uk>
2014-03-16 20:06:15 +0000

    Added boiler plate logging

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"log"
+	"os"
+)
+
+var info *log.Logger
+var debug *log.Logger
+
+func main() {
+	info = log.New(os.Stdout, "[info] ", log.Ltime)
+	info = log.New(os.Stdout, "[debug] ", log.Ltime|log.Lshortfile)
+}
