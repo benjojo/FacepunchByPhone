1bcc9a3b1d6fa8419cf070473f4387e35932725c
Ben Cox <ben@benjojo.co.uk>
2014-03-16 20:07:59 +0000

    Fixed actually use both logging interfaces.

--- main.go
+++ main.go
@@ -10,5 +10,8 @@ var debug *log.Logger
 
 func main() {
 	info = log.New(os.Stdout, "[info] ", log.Ltime)
-	info = log.New(os.Stdout, "[debug] ", log.Ltime|log.Lshortfile)
+	debug = log.New(os.Stdout, "[debug] ", log.Ltime|log.Lshortfile)
+
+	info.Println("Facepunch by phone, S16/03/2014")
+	debug.Println("Debug text enabled")
 }
