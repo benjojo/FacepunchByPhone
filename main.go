97b09614dff72b2eea1445d4c4f41d3e603adfc8
Ben Cox <Ben@Benjojo.co.uk>
2014-04-06 10:25:34 +0100

    Added some docs on the martini env stuff

--- main.go
+++ main.go
@@ -18,13 +18,18 @@ func main() {
 	ThreadCache = make(map[int][]Thread)
 	info.Println("Facepunch by phone, S16/03/2014")
 	debug.Println("Debug text enabled")
+
+	// Martini uses env vars to set its listening host and port,
+	// Here I am setting them. In case you where wondering 3223 is "FACE"
+	// On a mobile keypad :)
 	os.Setenv("HOST", "127.0.0.1")
 	os.Setenv("PORT", "3223")
+
 	m := martini.Classic()
 	m.Get("/incoming", newCaller)
 	m.Post("/incoming", newCaller)
 	m.Get("/sections", readSections)
-	m.Get("/threads/:handler", readThread)
+	m.Get("/threads/:handler", readThread) // Because threads shift around the :handler is needed.
 
 	m.Run()
 }
